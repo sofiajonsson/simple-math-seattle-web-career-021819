ec13a560e567009bc5e88f78ab928521494be7b7
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-02 03:55:38 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,18 +9,27 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
+  puts num1 / num2
   num1 / num2
 end
 
 def multiplication(num1, num2)
+  puts num1 * num2
+  num1 * num2
 end
 
 def modulo(num1, num2)
+  puts num1 % num2
+  num1 % num2
 end
 
 def square_root(num)
+  puts Math.sqrt(num)
+  Math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
   #Hint:  __ + (( __ * __ ) / __ )
+  puts num1 + ((num2 * num3) / num4)
+  num1 + ((num2 * num3) / num4)
 end
